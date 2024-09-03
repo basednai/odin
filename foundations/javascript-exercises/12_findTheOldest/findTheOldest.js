3e9bc6739b8932b6997bbf65941440be77679986
basednai <naiym.harris@icloud.com>
2024-09-02 21:33:58 -0400

    alternative solution to 12

--- foundations/javascript-exercises/12_findTheOldest/findTheOldest.js
+++ foundations/javascript-exercises/12_findTheOldest/findTheOldest.js
@@ -1,4 +1,20 @@
-const findTheOldest = function (people) {
+const people = [
+    {
+        name: "Carly",
+        yearOfBirth: 2018,
+    },
+    {
+        name: "Ray",
+        yearOfBirth: 1962,
+        yearOfDeath: 2011,
+    },
+    {
+        name: "Jane",
+        yearOfBirth: 1912,
+        yearOfDeath: 1941,
+    },
+]
+/* const findTheOldest = function (people) {
     const date = new Date()
     people.forEach(element => {
         if (!element.yearOfDeath) element.yearOfDeath = date.getFullYear()
@@ -8,6 +24,30 @@ const findTheOldest = function (people) {
     people.sort((a, b) => a.age - b.age)
 
     return(people.pop())
+}; */
+
+const getAge = (person) => {
+    if (!person.yearOfDeath) {
+        const date = new Date()
+        person.yearOfDeath = date.getFullYear()
+    }
+    return person.yearOfDeath - person.yearOfBirth
+}
+
+const findTheOldest = function (people) {
+
+    return people.reduce((oldest, current) => {
+        let oldestAge = getAge(oldest);
+        let currentAge = getAge(current)
+
+        return oldestAge > currentAge ? oldest : current
+    })
 };
+
+console.log(findTheOldest(people));
+
+console.log(people);
+
+
 // Do not edit below this line
 module.exports = findTheOldest;

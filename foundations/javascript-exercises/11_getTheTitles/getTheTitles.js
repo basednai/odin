cceb31a91efd8609609408dd8fca517a3ae57601
basednai <naiym.harris@icloud.com>
2024-09-06 09:53:39 -0400

    added completed exercises

--- foundations/javascript-exercises/11_getTheTitles/getTheTitles.js
+++ foundations/javascript-exercises/11_getTheTitles/getTheTitles.js
@@ -1,6 +1,28 @@
-const getTheTitles = function() {
+const books = [
+    {
+        title: 'Book',
+        author: 'Name'
+    },
+    {
+        title: 'Book2',
+        author: 'Name2'
+    }
+]
 
+/* const getTheTitles = function (books) {
+    let titles = []
+    books.forEach(book => {
+        titles.push(book.title)
+    });
+    return titles
+}; */
+
+const getTheTitles = function (books) {
+    return books.map((book) => book.title)
 };
+
+
+
 
 // Do not edit below this line
 module.exports = getTheTitles;
